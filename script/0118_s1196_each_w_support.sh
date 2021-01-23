68eefc1e1f31a603b3ec863e9bcc3583b04ee8a3
Shao-Wei <weiwei860222@gmail.com>
2021-01-23 17:57:58 +0800

    benchexec demo files

---
+++ script/0118_s1196_each_w_support.sh
@@ -0,0 +1,2 @@
+./../abc -c "compact -x 2 -l ../logs/0118_s1196.csv -e ../../benchmarks/ISCAS/s1196.blif ../careset/s1196.3000.c00.v01.careset.pla ../samples/s1196.3000.c00.v01"
+
---
+++ script/0118_s400_each_w_support.sh
@@ -0,0 +1,2 @@
+./../abc -c "compact -x 2 -l ../logs/0118_s400.csv -e ../../benchmarks/ISCAS/s400.blif ../careset/s400.3000.c00.v01.careset.pla ../samples/s400.3000.c00.v01"
+
